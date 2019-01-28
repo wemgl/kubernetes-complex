881920b4e06a1e8a7467da83dd80797c982c6c60
Wembley Leach <wembley.gl@gmail.com>
2019-01-27 19:45:06 -0500

    Add logging to k8s deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,17 @@
 names=("client" "server" "worker")
 
 for name in "${names[@]}"; do
+    echo "build ${name} image for commit ${GIT_COMMIT}"
     docker build -t wemgl/multi-${name}:latest -t wemgl/multi-${name}:${GIT_COMMIT} -f ./${name}/Dockerfile ./${name}
+    echo "push ${name} image for commit ${GIT_COMMIT}"
     docker push wemgl/multi-${name}:latest
     docker push wemgl/multi-${name}:${GIT_COMMIT}
 done
 
+echo "apply kubernetes configuration files"
 kubectl apply -f k8s
 
 for name in "${names[@]}"; do
+    echo "set ${name} image for commit ${GIT_COMMIT}"
     kubectl set image deployments/${name}-deployment ${name}=wemgl/multi-${name}:${GIT_COMMIT}
 done
